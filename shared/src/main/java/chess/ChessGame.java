016dba4ed95170fe22f7fadae2e7d3be6b5a985e
sims610 <sims610@byu.edu>
2025-10-03 18:40:28 -0600

    Implemented get and set methods for ChessGame

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,15 +10,18 @@ import java.util.Collection;
  */
 public class ChessGame {
 
-    public ChessGame() {
+    private TeamColor teamTurn = TeamColor.WHITE;
+    private ChessBoard board = new ChessBoard();
 
+    public ChessGame() {
+        board.resetBoard();
     }
 
     /**
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return teamTurn;
     }
 
     /**
@@ -27,7 +30,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        teamTurn = team;
     }
 
     /**
@@ -96,7 +99,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        this.board = board;
     }
 
     /**
@@ -105,6 +108,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return board;
     }
 }
--- shared/src/main/java/chess/ChessRuleBook.java
+++ shared/src/main/java/chess/ChessRuleBook.java
@@ -2,25 +2,25 @@ package chess;
 
 import java.util.Collection;
 
-public interface ChessRuleBook {
+public class ChessRuleBook {
 
-    default Collection<ChessMove> validMoves(ChessBoard board, ChessPosition myPosition) {
+    Collection<ChessMove> validMoves(ChessBoard board, ChessPosition myPosition) {
         throw new RuntimeException("Not implemented");
     }
 
-    default Boolean isBoardValid(ChessBoard board) {
+    Boolean isBoardValid(ChessBoard board) {
         throw new RuntimeException("Not implemented");
     }
 
-    default Boolean isInCheck(ChessBoard board, ChessGame.TeamColor color) {
+    Boolean isInCheck(ChessBoard board, ChessGame.TeamColor color) {
         throw new RuntimeException("Not implemented");
     }
 
-    default Boolean isInCheckmate(ChessBoard board, ChessGame.TeamColor color) {
+    Boolean isInCheckmate(ChessBoard board, ChessGame.TeamColor color) {
         throw new RuntimeException("Not implemented");
     }
 
-    default Boolean isInStalemate(ChessBoard board, ChessGame.TeamColor color) {
+    Boolean isInStalemate(ChessBoard board, ChessGame.TeamColor color) {
         throw new RuntimeException("Not implemented");
     }
 
