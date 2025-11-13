8e7aeafc86cd6496adfc835a47913533894c33fb
sims610 <sims610@byu.edu>
2025-11-13 11:39:30 -0700

    Created Login and Register methods on ServerFacade

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,28 +1,100 @@
 package client;
 
-import model.requestresult.LoginRequest;
-import model.requestresult.LoginResult;
+import com.google.gson.Gson;
+import model.requestresult.*;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
 import java.util.Locale;
 
 public class ServerFacade {
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final String serverUrl;
 
-    private final HttpClient httpClient = HttpClient.newHttpClient();
+    public ServerFacade(String host, int port) {
+        serverUrl = String.format(Locale.getDefault(), "http://%s:%d", host, port);
+    }
 
-    public void doGet(String host, int port, String urlPath) throws URISyntaxException, IOException, URISyntaxException {
+    public void get(String host, int port, String urlPath) throws Exception {
         String urlString = String.format(Locale.getDefault(), "http://%s:%d%s", host, port, urlPath);
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(new URI(urlString))
+                .timeout(java.time.Duration.ofMillis(5000))
                 .GET()
                 .build();
 
     }
 
-    LoginResult login(LoginRequest loginRequest) {return null;}
+    RegisterResult register(RegisterRequest registerRequest) {
+        var request = buildRequest("POST", "/user", registerRequest);
+        var response = sendRequest(request);
+        return handleResponse(response, RegisterResult.class);
+    }
+
+    LoginResult login(LoginRequest loginRequest) {
+        var request = buildRequest("POST", "/session", loginRequest);
+        var response = sendRequest(request);
+        return handleResponse(response, LoginResult.class);
+    }
+
+    LogoutResult logout(LogoutRequest logoutRequest) {return null;}
+
+    CreateResult create(CreateRequest createRequest) {return null;}
+
+    ListResult listGames(ListRequest listRequest) {return null;}
+
+    JoinResult joinGame(JoinRequest joinRequest) {return null;}
+
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .timeout(java.time.Duration.ofMillis(5000))
+                .method(method, makeRequestBody(body));
+        if (body != null) {
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
+
+    private BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return BodyPublishers.noBody();
+        }
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) {
+        try {
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            throw new RuntimeException("Didn't work");
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) {
+        var status = response.statusCode();
+        if (!isSuccessful(status)) {
+            var body = response.body();
+            if (body != null) {
+                throw new RuntimeException("Didn't work");
+            }
+            throw new RuntimeException("Status code failure");
+        }
+        if (responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+        return null;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
 }
--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,23 +1,51 @@
 package client;
 
+import dataaccess.*;
+import model.AuthData;
+import model.UserData;
+import model.requestresult.LoginRequest;
+import model.requestresult.LoginResult;
+import model.requestresult.RegisterRequest;
+import model.requestresult.RegisterResult;
 import org.junit.jupiter.api.*;
 import server.Server;
+
+import javax.xml.crypto.Data;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade serverFacade;
+    private static AuthDAO authDAO;
+    private static GameDAO gameDAO;
+    private static UserDAO userDAO;
+    private static RegisterRequest newUser;
+    private static LoginRequest loginUser;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        serverFacade = new ServerFacade("localhost", port);
+        authDAO = new MySQLAuthDAO();
+        gameDAO = new MySQLGameDAO();
+        userDAO = new MySQLUserDAO();
+        newUser = new RegisterRequest("newUser", "newUserPassword", "newUserEmail");
+        loginUser = new LoginRequest("newUser", "newUserPassword");
     }
 
     @AfterAll
     static void stopServer() {
         server.stop();
+    }
+
+    @BeforeEach
+    void clearDB() throws DataAccessException {
+        authDAO.clear();
+        gameDAO.clear();
+        userDAO.clear();
     }
 
 
@@ -26,4 +54,35 @@ public class ServerFacadeTests {
         Assertions.assertTrue(true);
     }
 
+    @Test
+    public void registerPassesTest() {
+        RegisterResult result = serverFacade.register(newUser);
+        Assertions.assertNotNull(result.authToken());
+    }
+
+    @Test
+    public void registerFailsTest() {
+        // Tries to register a user twice
+        serverFacade.register(newUser);
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            serverFacade.register(newUser);
+        });
+    }
+
+    @Test
+    public void loginPassesTest() throws DataAccessException {
+        serverFacade.register(newUser);
+        LoginResult result = serverFacade.login(loginUser);
+        AuthData expect = authDAO.read(result.authToken());
+        Assertions.assertNotNull(result.authToken());
+        Assertions.assertEquals(expect.username(), result.username());
+    }
+
+    @Test
+    public void loginFailsTest() {
+        // Tries to login a user that is not registered
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            serverFacade.login(loginUser);
+        });
+    }
 }
