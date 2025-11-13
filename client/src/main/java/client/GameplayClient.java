632135f3d51d7767ff1426c2e0abe2683c9d1c2c
sims610 <sims610@byu.edu>
2025-11-13 08:39:39 -0700

    Created general structure for Gameplay Client

--- client/src/main/java/client/GameplayClient.java
+++ client/src/main/java/client/GameplayClient.java
@@ -1,4 +1,52 @@
 package client;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class GameplayClient {
+    public void run() {
+        printBoard();
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = eval(line);
+                System.out.print(result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + "[GAME] >>> ");
+    }
+
+    public String eval(String input) {
+        try {
+            String[] tokens = input.toLowerCase().split(" ");
+            String cmd = (tokens.length > 0) ? tokens[0] : "help";
+            String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (Throwable e) {
+            return e.getMessage();
+        }
+    }
+
+    private String help() {
+        throw new RuntimeException("Not Implemented");
+    }
+
+    private void printBoard() {
+        throw new RuntimeException("Not implemented");
+    }
 }
