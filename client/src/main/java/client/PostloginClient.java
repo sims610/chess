a66e70b73cb973b124df3fdc276d6bcf941abc6b
sims610 <sims610@byu.edu>
2025-11-14 02:56:33 -0700

    Implemented ListGames in PostLoginClient

--- client/src/main/java/client/PostloginClient.java
+++ client/src/main/java/client/PostloginClient.java
@@ -1,9 +1,10 @@
 package client;
 
+import model.GameData;
+import model.requestresult.*;
+
 import java.util.Arrays;
 import java.util.Scanner;
-
-import static ui.EscapeSequences.WHITE_QUEEN;
 
 public class PostloginClient {
     private ServerFacade serverFacade;
@@ -22,6 +23,9 @@ public class PostloginClient {
             try {
                 result = eval(line);
                 System.out.print(result);
+                if (result.startsWith("logged out")) {
+                    return;
+                }
             } catch (Throwable e) {
                 var msg = e.toString();
                 System.out.print(msg);
@@ -54,11 +58,22 @@ public class PostloginClient {
     }
 
     private String create(String... params) {
-        throw new RuntimeException("Not Implemented");
+        if (params.length >= 1) {
+            CreateRequest createRequest = new CreateRequest(params[0]);
+            CreateResult result = serverFacade.create(createRequest);
+            return String.format("created game: %s", params[0]);
+        }
+        throw new RuntimeException("couldn't create game");
     }
 
     private String list() {
-        throw new RuntimeException("Not Implemented");
+        ListResult result = serverFacade.listGames(new ListRequest());
+        StringBuilder total = new StringBuilder();
+        for (int i = 0; i < result.games().size(); i++) {
+            GameData game = result.games().get(i);
+            total.append(String.format("%d: %s White: %s, Black: %s\n", i + 1, game.gameName(), game.whiteUsername(), game.blackUsername()));
+        }
+        return total.toString();
     }
 
     private String join(String... params) {
@@ -70,10 +85,19 @@ public class PostloginClient {
     }
 
     private String logout() {
-        throw new RuntimeException("Not Implemented");
+        serverFacade.logout(new LogoutRequest(null));
+        return "logged out";
     }
 
     private String help() {
-        throw new RuntimeException("Not Implemented");
+        return """
+                create <NAME> - a game
+                list - games
+                join <ID> [WHITE|BLACK] - a game
+                observe <ID> - a game
+                logout - when you are done
+                quit - playing chess
+                help - with possible commands
+                """;
     }
 }
